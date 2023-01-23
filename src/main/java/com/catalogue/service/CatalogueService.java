910523a99975ebe7fd7249a6b9099903f5d57543
theanuragharsh <nrgharsh@gmail.com>
2023-01-23 19:52:38 +0530

    created Get Catalogue Items API

--- src/main/java/com/catalogue/service/CatalogueService.java
+++ src/main/java/com/catalogue/service/CatalogueService.java
@@ -1,5 +1,9 @@
 package com.catalogue.service;
 
+import com.catalogue.models.CatalogueItem;
+import reactor.core.publisher.Flux;
+
 public interface CatalogueService {
+
+    Flux<CatalogueItem> getCatalogueItems();
 }
-""
